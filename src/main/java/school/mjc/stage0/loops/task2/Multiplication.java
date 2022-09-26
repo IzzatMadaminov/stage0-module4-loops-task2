cf415c8ba202b4e8928da173445f31b786923cc5
Izzat Madaminov <izzat.madaminov@relentless.agency>
2022-09-26 22:15:50 +0500

    Task #2. While Loops FIX

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int i = 1;
-        while (i <= Math.abs(multiplyByAndToInclusive)) {
+        int i = 0;
+        while (multiplyByAndToInclusive != 0 && i <= Math.abs(multiplyByAndToInclusive)) {
             System.out.println(i * multiplyByAndToInclusive);
             i++;
         }
