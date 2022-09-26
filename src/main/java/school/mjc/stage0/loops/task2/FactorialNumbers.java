68c179794605668547e43f4eb5d87a2d9127b5cd
Izzat Madaminov <izzat.madaminov@relentless.agency>
2022-09-26 22:10:20 +0500

    Task #2. While Loops

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int i = 0;
+        while (i <= printToInclusive) {
+            System.out.println(factorial(i));
+            i++;
+        }
+    }
 
+    private int factorial(int i) {
+        int p = 1;
+        int j = 1;
+        if (i == 0) {
+            return 1;
+        } else {
+            while (j <= i) {
+                p *= j;
+                j++;
+            }
+        }
+        return p;
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,28 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 0;
+        while (i <= printToInclusive) {
+            if (isPrime(i))
+                System.out.println(i);
+            i++;
+        }
+    }
+
+    private boolean isPrime(int i) {
+        if (i < 2) {
+            return false;
+        }
+        if (i == 2) {
+            return true;
+        }
+        if (i % 2 == 0)
+            return false;
+        for (int j = 3; j * j <= i; j += 2) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
